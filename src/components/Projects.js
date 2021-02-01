0b6d8ada20cda86bf78d22bd9766aa8c84babd27
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-02-01 17:56:24 -0600

    Small changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,10 +11,12 @@ function Projects() {
                     <ul className="cards__items">
                         <ProjectItems src='images/Space-Race.png' text='Space themed game that captures the users input to move a rocket around asteroids' label='Space Race' path='https://jaxonnarramore.github.io/' />
                         <ProjectItems src='images/Air-Qual.png' text='Get air quality information for anywhere in the world' label='Air Qual' path='https://air-qual.herokuapp.com/' />
+                        <ProjectItems src='images/PDR.png' text='Track bugs on your website as a user, developer or a company admin.' label='PDR' path='https://pestdamagereport.herokuapp.com/' />
                     </ul>
                     <ul className="cards__items">
-                        <ProjectItems src='images/PDR.png' text='Track bugs on your website as a user, developer or a company admin.' label='PDR' path='https://pestdamagereport.herokuapp.com/' />
-                        <ProjectItems src='images/PDR.png' text='This is the text for this ' label='Task Manager' path='https://pestdamagereport.herokuapp.com/' />
+                        <ProjectItems src='' text='This is the text for this ' label='Task Manager' path='https://pestdamagereport.herokuapp.com/' />
+                        <ProjectItems src='https://www.nopcommerce.com/images/thumbs/0005720_coming-soon-page_550.jpeg' text='COMING SOON' label='COMING SOON' path='' />
+                        <ProjectItems src='https://www.nopcommerce.com/images/thumbs/0005720_coming-soon-page_550.jpeg' text='COMING SOON' label='COMING SOON' path='' />
                     </ul>
                 </div>
             </div>
